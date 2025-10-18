727001d655add1e9c47c9a1fcc175c64d0e70a81
REX <ffconfig.op@gmail.com>
2025-10-18 11:07:06 +0600

    Add webconfig.js for application configuration

---
+++ web/webconfig.js
@@ -0,0 +1,9 @@
+{
+    "discordServerLink": "https://discord.gg/codexstudio",
+    "contactEmail": "info@codexstudio.com",
+    "botLogs": {
+        "enabled": false,
+        "logUrl": "/logs",
+        "description": "View detailed logs for our bots' activities and performance."
+    }
+}
